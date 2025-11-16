4a8f491b84557e7abffab6d0b0193730450bc8af
Brian <brianramirezdelgadodev@gmail.com>
2025-11-16 18:00:30 +0000

    mejoras de rendimiento en lenis

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import '@/App.css';
 import 'lenis/dist/lenis.css';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import Portfolio from '@/pages/Portfolio';
-import Lenis from 'lenis';
 import { useEffect } from 'react';
 import { ReactLenis } from '@/utils/lenis';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 
 function App() {
     useEffect(() => {
-        const lenis = new Lenis({ anchors: true });
-        function raf(time: any) {
-            lenis.raf(time);
+        // Solo ejecutar cuando el navegador está libre
+        requestIdleCallback(async () => {
+            const { default: Lenis } = await import('lenis');
+            const lenis = new Lenis({ anchors: true });
+
+            function raf(time: any) {
+                lenis.raf(time);
+                requestAnimationFrame(raf);
+            }
+
             requestAnimationFrame(raf);
-        }
-        requestAnimationFrame(raf);
+        });
     }, []);
+
     return (
         <ThemeProvider defaultTheme="system" storageKey="ui-theme">
             <ReactLenis root />
